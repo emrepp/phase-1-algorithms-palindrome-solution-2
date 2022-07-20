bd63861a55448c499b0ca28000a55d6a752b76a3
Emma Repp <emmarepp.draws@gmail.com>
2022-07-19 20:44:15 -1000

    commit tree 1f6cc59bdc982c7fd606b1b98d844adffc42d66b

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
+  for(let i = 0; i  < word.length / 2; i++){
+    const j = word.length - 1 - i;
+    const startChar = word[i];
+    const endChar = word[j];
+    if (startChar !== endChar) return false;
+
+  }
+return true
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+  Iterate from the first letter to the middle letter and check each letter
+  from the corresponding last letter to the middle letter
+  if any letters don't match, return false
+  if all letters match, return true
+
 */
 
 /*
